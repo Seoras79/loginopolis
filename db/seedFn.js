17c1ef59149106c95ef02b0587e9852e42ee5e75
Seoras79 <seorasproductions@gmail.com>
2023-04-12 12:16:18 +0100

    added routes and hashed users

--- db/seedFn.js
+++ db/seedFn.js
@@ -1,10 +1,22 @@
 const {sequelize} = require('./db');
 const {User} = require('./');
 const users = require('./seedData');
+const bcrypt = require('bcrypt')
 
 const seed = async () => {
   await sequelize.sync({ force: true }); // recreate db
-  await User.bulkCreate(users);
+  // await User.bulkCreate(users);
+  const hashedUsers = await Promise.all(
+    users.map(async (user) => {
+      const hashedPassword = await bcrypt.hash(user.password, 10)
+      return {
+        ...user,
+        password:hashedPassword
+      };
+    })
+  );
+  await User.bulkCreate(hashedUsers);
 };
+
 
 module.exports = seed;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { User } = require('./db');
+const bcrypt = require('bcrypt')
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -14,11 +15,39 @@ app.get('/', async (req, res, next) => {
   }
 });
 
+
+
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+
+app.post('/register', async (req, res, next) => {
+  try {
+    const SALT_LENGTH = 10;
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, SALT_LENGTH);
+    await User.create({ username, password: hashedPassword});
+    res.send("Success");
+    
+  } catch (error) {
+    console.error(error);
+    next(error)
+  }
+})
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
 
+app.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const  user = await User.findOne({ where: { username } });
+    if (!user) return;
+    const matches =await bcrypt.compare(password, user.password);
+    res.send(matches ? "Login" : "incorrect username or password");
+  } catch (error) {
+    console.error(error);
+    next(error)
+  }
+})
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
